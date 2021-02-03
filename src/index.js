368544ce347845de3b5ee964001524b9c2d594e6
Sakshi Sonawane <sonawanesakshi610@gmail.com>
2021-02-03 08:47:07 +0530

    class method

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
+import Classmethod from  './Classmethod.js';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Classmethod />
   </React.StrictMode>,
   document.getElementById('root')
 );
